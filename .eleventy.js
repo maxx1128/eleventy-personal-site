bb200ea9387242c95336c6293b966afd80a2dd5f
Max Antonucci <maxx1128@gmail.com>
2020-08-26 11:09:12 -0400

    Use custom filter for post read time

--- .eleventy.js
+++ .eleventy.js
@@ -28,6 +28,18 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('LLLL d, yyyy');
   });
 
+  eleventyConfig.addFilter("postReadTime", content => {
+    const isRejectedCharacter = (str) => str.length === 1 && !str.toLowerCase().match(/[a-z]/i);
+
+    const acceptedContent = content
+                              .split(" ")
+                              .filter(content => content !== "" && !isRejectedCharacter(content)),
+          readTime = Math.round(acceptedContent.length / 180);
+
+    if (readTime < 2) { return 2; }
+    return readTime;
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
