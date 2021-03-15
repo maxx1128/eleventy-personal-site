6b611d088e46234634b825b104e1a8bdd813bef4
Max Antonucci <maxx1128@gmail.com>
2021-03-15 18:58:09 -0400

    Random link JS refactors

--- _javascript/randomLinkButton.js
+++ _javascript/randomLinkButton.js
@@ -1,21 +1,14 @@
 (function() {
-  const posts = window.ContentData.posts,
-        randomPost = posts[Math.floor(Math.random() * posts.length)],
-        postLinkElement = document.getElementById('random-post-link');
+  const setRandomPostLink = (content, linkId) => {
+    const randomPost = content[Math.floor(Math.random() * content.length)],
+          postLinkElement = document.getElementById(linkId);
 
-  if (postLinkElement) {
-    postLinkElement.classList.remove('hidden');
-    postLinkElement.setAttribute('href', randomPost.url);
+    if (postLinkElement) {
+      postLinkElement.classList.remove('hidden');
+      postLinkElement.setAttribute('href', randomPost.url);
+    }
   }
-})();
 
-(function() {
-  const notes = window.ContentData.notes,
-        randomNote = notes[Math.floor(Math.random() * notes.length)],
-        noteLinkElement = document.getElementById('random-note-link');
-
-  if (noteLinkElement) {
-    noteLinkElement.classList.remove('hidden');
-    noteLinkElement.setAttribute('href', randomNote.url);
-  }
+  setRandomPostLink(window.ContentData.posts, 'random-post-link');
+  setRandomPostLink(window.ContentData.notes, 'random-note-link');
 })();
