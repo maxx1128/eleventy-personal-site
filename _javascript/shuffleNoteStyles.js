e63c6cc8e884b90cb32d309f89a0beda24d1659b
Max Antonucci <maxx1128@gmail.com>
2021-04-25 22:31:17 -0400

    Add JS note doodle shuffling

--- _javascript/shuffleNoteStyles.js
+++ _javascript/shuffleNoteStyles.js
@@ -1,28 +1,66 @@
+const doodleViewboxes = [
+  "0 0 79.5 63.5",   // Espeon
+  "0 0 160 150", // Margaret
+  "0 0 100 142"  // Winter hat
+];
+
 (function() {
-  const noteElements = document.getElementsByClassName('note-styler');
+  const noteLimit = 15;
+  const getIndexes = (arrayLength, itemLimit) => shuffle([...Array(arrayLength).keys()]).map(index => index % itemLimit);
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
 
-  function removeNoteStyleClass(el) {
-    var regx = new RegExp('\\b' + 'note-coloring--' + '.?\\b', 'g');
-    el.className = el.className.replace(regx, '');
+    return array;
   }
 
-  if (noteElements) {
-    const shuffle = (array) => {
-      for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+  const shuffleColoringClasses = () => {
+    const elements = document.getElementsByClassName('note-styler');
+
+    function removeElStyleClass(el) {
+      var regx = new RegExp('\\b' + 'note-coloring' + '--' + '.?\\b', 'g');
+
+      el.className = el.className.replace(regx, '');
+    }
+
+    if (elements) {
+      const classIndexes = getIndexes(noteLimit, noteLimit);
+
+      for (let el of elements) {
+        const randomIndex = classIndexes[0];
+
+        removeElStyleClass(el);
+        el.classList.add(`note-coloring--${randomIndex}`);
+        classIndexes.shift();
       }
-
-      return array;
-    }
-
-    const numberOfPalettes = 15,
-          classIndexes     = shuffle([...Array(numberOfPalettes).keys()]);
-
-    for (let note of noteElements) {
-      removeNoteStyleClass(note);
-      note.classList.add(`note-coloring--${classIndexes[0]}`);
-      classIndexes.shift();
     }
   }
+
+  const shuffleDoodleImages = () => {
+    const doodleLimit = doodleViewboxes.length;
+    const elements = document.getElementsByClassName('note-doodle__use');
+
+    if (elements) {
+      const classIndexes = getIndexes(noteLimit * 6, doodleLimit);
+
+      for (let el of elements) {
+        const randomIndex = classIndexes[0];
+        el.parentElement.classList.remove("hidden");
+
+        if (Math.random() < 0.2) {
+          el.parentElement.classList.add("hidden");
+        }
+
+        el.parentElement.setAttribute('viewBox', doodleViewboxes[randomIndex]);
+        el.setAttribute('href', `/assets/images/doodles/all.svg#doodle-${randomIndex}`);
+        classIndexes.shift();
+      }
+    }
+  }
+
+  shuffleColoringClasses();
+  // TODO: enable once more images are added in
+  // shuffleDoodleImages();
 })();
