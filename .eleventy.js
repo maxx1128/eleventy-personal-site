d779bc7a80ce1ffb6e38169aec9b23b14aaf15db
Max Antonucci <maxx1128@gmail.com>
2021-12-03 14:48:47 -0500

    Add a11y-footnotes plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,218 +1,232 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
-const moment = require("moment");
+const { DateTime } = require('luxon')
+const footnotes = require('eleventy-plugin-footnotes')
+const fs = require('fs')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+const pluginNavigation = require('@11ty/eleventy-navigation')
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+const moment = require('moment')
 
 const hideFutureItems = (item) => {
   const now = new Date(),
-        postDate = item.date,
-        wasPublishedLater = postDate < now;
+    postDate = item.date,
+    wasPublishedLater = postDate < now
 
-  return wasPublishedLater;
+  return wasPublishedLater
 }
 
 const sortByDate = (a, b) => {
-  if (a.date < b.date) { return -1; }
-  if (a.date > b.date) { return 1; }
-  return 0;
+  if (a.date < b.date) {
+    return -1
+  }
+  if (a.date > b.date) {
+    return 1
+  }
+  return 0
 }
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginSyntaxHighlight)
+  eleventyConfig.addPlugin(pluginNavigation)
+  eleventyConfig.addPlugin(footnotes)
 
-  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true)
 
-  eleventyConfig.addFilter("addNbsp", (str) => {
+  eleventyConfig.addFilter('addNbsp', (str) => {
     if (!str) {
-      return;
+      return
     }
-    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
-    title = title.replace(/"(.*)"/g, '\\"$1\\"');
-    return title;
-  });
+    let title = str.replace(/((.*)\s(.*))$/g, '$2&nbsp;$3')
+    title = title.replace(/"(.*)"/g, '\\"$1\\"')
+    return title
+  })
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-  });
+  eleventyConfig.addFilter('readableDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy')
+  })
 
-  eleventyConfig.addFilter("noteDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('LLLL d, yyyy');
-  });
+  eleventyConfig.addFilter('noteDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
+      'LLLL d, yyyy',
+    )
+  })
 
-  eleventyConfig.addFilter("postReadTime", content => {
-    const isRejectedCharacter = (str) => str.length === 1 && !str.toLowerCase().match(/[a-z]/i);
+  eleventyConfig.addFilter('postReadTime', (content) => {
+    const isRejectedCharacter = (str) =>
+      str.length === 1 && !str.toLowerCase().match(/[a-z]/i)
 
     const acceptedContent = content
-                              .split(" ")
-                              .filter(content => content !== "" && !isRejectedCharacter(content)),
-          readTime = Math.round(acceptedContent.length / 180);
+        .split(' ')
+        .filter((content) => content !== '' && !isRejectedCharacter(content)),
+      readTime = Math.round(acceptedContent.length / 180)
 
-    if (readTime < 2) { return 2; }
-    return readTime;
-  });
+    if (readTime < 2) {
+      return 2
+    }
+    return readTime
+  })
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
-
-  eleventyConfig.addLiquidFilter("toUTCString", (date) => {
-    const utc = new Date(date).toUTCString();
-    return moment.utc(utc).format("MMMM D, YYYY");
-  });
-
-  eleventyConfig.addLiquidFilter("toUTCDatePath", (date) => {
-    const utc = new Date(date).toUTCString();
-    return moment.utc(utc).format("YYYY/MM/DD/");
-  });
-
-  eleventyConfig.addLiquidFilter("onlyPublished", (posts) => {
-    return posts.filter(hideFutureItems);
-  });
-
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
-
-    return array.slice(0, n);
-  });
-
-  eleventyConfig.addCollection("tagList", function(collection) {
-    let tagSet = new Set();
-    collection.getAll().forEach(function(item) {
-      if( "tags" in item.data ) {
-        let tags = item.data.tags;
-
-        tags = tags.filter(function(item) {
-          switch(item) {
-            // this list should match the `filter` list in tags.njk
-            case "all":
-            case "nav":
-            case "post":
-            case "posts":
-              return false;
-          }
-
-          return true;
-        });
-
-        for (const tag of tags) {
-          tagSet.add(tag);
-        }
-      }
-    });
-
-    // returning an array in addCollection works in Eleventy 0.5.3
-    return [...tagSet];
-  });
-
-  eleventyConfig.addCollection("TILCategories", function(collection) {
-    let categorySet = new Set();
-    collection.getAll().forEach(function(item) {
-      if( "contentType" in item.data && item.data.contentType === "Today I Learned" ) {
-        let category = item.data.category;
-        categorySet.add(category);
-      }
-    });
-
-    // returning an array in addCollection works in Eleventy 0.5.3
-    return [...categorySet].sort();
-  });
-
-  eleventyConfig.addCollection("posts", function (collection) {
-    return collection.getFilteredByGlob("./posts/*.md")
-      .filter(hideFutureItems)
-      .reverse();
-  });
-
-  eleventyConfig.addCollection("notes", function (collection) {
-    return collection.getFilteredByGlob("./notes/*.md")
-      .filter(hideFutureItems)
-      .reverse();
-  });
-
-  eleventyConfig.addCollection("todayILearned", function (collection) {
-    return collection.getFilteredByGlob("./todayILearned/*.md")
-      .filter(hideFutureItems)
-      .sort(sortByDate);
-  });
-
-  eleventyConfig.addCollection("reversedTIL", function (collection) {
-    return collection.getFilteredByGlob("./todayILearned/*.md")
-      .filter(hideFutureItems)
-      .sort(sortByDate)
-      .reverse();
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd')
   })
 
-  eleventyConfig.addCollection("feed", function (collection) {
+  eleventyConfig.addLiquidFilter('toUTCString', (date) => {
+    const utc = new Date(date).toUTCString()
+    return moment.utc(utc).format('MMMM D, YYYY')
+  })
+
+  eleventyConfig.addLiquidFilter('toUTCDatePath', (date) => {
+    const utc = new Date(date).toUTCString()
+    return moment.utc(utc).format('YYYY/MM/DD/')
+  })
+
+  eleventyConfig.addLiquidFilter('onlyPublished', (posts) => {
+    return posts.filter(hideFutureItems)
+  })
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter('head', (array, n) => {
+    if (n < 0) {
+      return array.slice(n)
+    }
+
+    return array.slice(0, n)
+  })
+
+  eleventyConfig.addCollection('tagList', function (collection) {
+    let tagSet = new Set()
+    collection.getAll().forEach(function (item) {
+      if ('tags' in item.data) {
+        let tags = item.data.tags
+
+        tags = tags.filter(function (item) {
+          switch (item) {
+            // this list should match the `filter` list in tags.njk
+            case 'all':
+            case 'nav':
+            case 'post':
+            case 'posts':
+              return false
+          }
+
+          return true
+        })
+
+        for (const tag of tags) {
+          tagSet.add(tag)
+        }
+      }
+    })
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...tagSet]
+  })
+
+  eleventyConfig.addCollection('TILCategories', function (collection) {
+    let categorySet = new Set()
+    collection.getAll().forEach(function (item) {
+      if (
+        'contentType' in item.data &&
+        item.data.contentType === 'Today I Learned'
+      ) {
+        let category = item.data.category
+        categorySet.add(category)
+      }
+    })
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...categorySet].sort()
+  })
+
+  eleventyConfig.addCollection('posts', function (collection) {
+    return collection
+      .getFilteredByGlob('./posts/*.md')
+      .filter(hideFutureItems)
+      .reverse()
+  })
+
+  eleventyConfig.addCollection('notes', function (collection) {
+    return collection
+      .getFilteredByGlob('./notes/*.md')
+      .filter(hideFutureItems)
+      .reverse()
+  })
+
+  eleventyConfig.addCollection('todayILearned', function (collection) {
+    return collection
+      .getFilteredByGlob('./todayILearned/*.md')
+      .filter(hideFutureItems)
+      .sort(sortByDate)
+  })
+
+  eleventyConfig.addCollection('reversedTIL', function (collection) {
+    return collection
+      .getFilteredByGlob('./todayILearned/*.md')
+      .filter(hideFutureItems)
+      .sort(sortByDate)
+      .reverse()
+  })
+
+  eleventyConfig.addCollection('feed', function (collection) {
     const allContent = [
-      ...collection.getFilteredByGlob("./notes/*.md"),
-      ...collection.getFilteredByGlob("./posts/*.md"),
-      ...collection.getFilteredByGlob("./todayILearned/*.md")
+      ...collection.getFilteredByGlob('./notes/*.md'),
+      ...collection.getFilteredByGlob('./posts/*.md'),
+      ...collection.getFilteredByGlob('./todayILearned/*.md'),
     ].sort(sortByDate)
 
     return allContent
       .filter(hideFutureItems)
       .slice(Math.max(allContent.length - 50, 1))
-      .reverse();
-  });
+      .reverse()
+  })
 
-  eleventyConfig.addCollection("recent_notes", function (collection) {
-    return collection.getFilteredByGlob("./notes/*.md")
+  eleventyConfig.addCollection('recent_notes', function (collection) {
+    return collection
+      .getFilteredByGlob('./notes/*.md')
       .filter(hideFutureItems)
       .reverse()
-      .slice(0, 2);
-  });
+      .slice(0, 2)
+  })
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("assets/**/*");
-  eleventyConfig.addPassthroughCopy("serviceworker.js");
-  eleventyConfig.addPassthroughCopy("manifest.webmanifest");
+  eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('assets/**/*')
+  eleventyConfig.addPassthroughCopy('serviceworker.js')
+  eleventyConfig.addPassthroughCopy('manifest.webmanifest')
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
+    linkify: true,
   }).use(markdownItAnchor, {
     permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+    permalinkClass: 'direct-link',
+    permalinkSymbol: '#',
+  })
+  eleventyConfig.setLibrary('md', markdownLibrary)
 
   // Browsersync Overrides
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync('_site/404.html')
 
-        browserSync.addMiddleware("*", (req, res) => {
+        browserSync.addMiddleware('*', (req, res) => {
           // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
+          res.write(content_404)
+          res.end()
+        })
       },
     },
     ui: false,
-    ghostMode: false
-  });
+    ghostMode: false,
+  })
 
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    templateFormats: ['md', 'njk', 'html', 'liquid'],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about those.
@@ -224,16 +238,16 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
     // pathPrefix: "/",
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+    markdownTemplateEngine: 'liquid',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
 
     // These are all optional, defaults are shown:
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
-};
+      input: '.',
+      includes: '_includes',
+      data: '_data',
+      output: '_site',
+    },
+  }
+}
--- serviceworker.js
+++ serviceworker.js
@@ -1,62 +1,61 @@
-const version         = 'V1.91',
-      staticCacheName = `${version}staticfiles`;
+const version = 'V1.92',
+  staticCacheName = `${version}staticfiles`
 
-addEventListener('install', installEvent => {
-  skipWaiting();
+addEventListener('install', (installEvent) => {
+  skipWaiting()
   installEvent.waitUntil(
-    caches.open(staticCacheName)
-      .then(staticCache => {
-        // Nice to Cache
-        staticCache.addAll([
-          'about/index.html',
-          '404.html',
-          'index.html',
-          'notes/index.html',
-          'now/index.html',
-          'writing/index.html',
-          '/assets/images/global/header-default.jpg',
-          '/assets/images/global/header-dark.jpg',
-          '/assets/images/global/favicon.ico',
-          '/assets/images/global/profile.png',
-          '/assets/images/icons/icon-192.png',
-          '/assets/images/icons/icon-512.png',
-          '/assets/images/doodles/all.svg',
-        ]);
+    caches.open(staticCacheName).then((staticCache) => {
+      // Nice to Cache
+      staticCache.addAll([
+        'about/index.html',
+        '404.html',
+        'index.html',
+        'notes/index.html',
+        'now/index.html',
+        'writing/index.html',
+        '/assets/images/global/header-default.jpg',
+        '/assets/images/global/header-dark.jpg',
+        '/assets/images/global/favicon.ico',
+        '/assets/images/global/profile.png',
+        '/assets/images/icons/icon-192.png',
+        '/assets/images/icons/icon-512.png',
+        '/assets/images/doodles/all.svg',
+      ])
 
-        // Need to Cache
-        return staticCache.addAll([
-          '/assets/css/main.css',
-          '/assets/javascript/main.js'
-        ]);
-      })
-  );
-});
+      // Need to Cache
+      return staticCache.addAll([
+        '/assets/css/main.css',
+        '/assets/javascript/main.js',
+      ])
+    }),
+  )
+})
 
 // For clearing out old caches
-addEventListener('activate', activateEvent => {
+addEventListener('activate', (activateEvent) => {
   activateEvent.waitUntil(
     // Clean the old caches up
-    caches.keys()
-      .then(cacheNames => {
+    caches
+      .keys()
+      .then((cacheNames) => {
         return Promise.all(
-          cacheNames.map(cacheName => {
+          cacheNames.map((cacheName) => {
             if (cacheName !== staticCacheName) {
               // Time to kill!
-              return caches.delete(cacheName);
+              return caches.delete(cacheName)
             }
-          })
-        );
+          }),
+        )
       })
-      .then(() => clients.claim())
-  );
-});
+      .then(() => clients.claim()),
+  )
+})
 
-addEventListener('fetch', fetchEvent => {
-  const request = fetchEvent.request;
+addEventListener('fetch', (fetchEvent) => {
+  const request = fetchEvent.request
   fetchEvent.respondWith(
-    caches.match(request)
-      .then(responseFromCache => {
-        return responseFromCache || fetch(request);
-      })
-  );
-});
+    caches.match(request).then((responseFromCache) => {
+      return responseFromCache || fetch(request)
+    }),
+  )
+})
