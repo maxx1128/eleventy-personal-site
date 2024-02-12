954e1f4d8ae1314ecc1b89309477790f51182e8f
Max Antonucci <maxx1128@gmail.com>
2024-02-12 16:00:22 -0500

    Use parent selector for theme switcher

--- _javascript/themeSwitcher.js
+++ _javascript/themeSwitcher.js
@@ -1,6 +1,5 @@
 (function() {
-  const themeRadios = document.getElementsByClassName('theme-switcher__input'),
-        allThemes = ['light', 'dark'];
+  const themeRadios = document.getElementsByClassName('theme-switcher__input');
 
   function addEventListeners() {
     for (var i = 0, max = themeRadios.length; i < max; i++) {
@@ -15,16 +14,6 @@
     const themeRadio = document.getElementById(`theme-${theme}`);
     themeRadio.checked = true;
     localStorage.setItem('theme', theme);
-    replaceHtmlClasses(theme);
-  }
-
-  function replaceHtmlClasses(theme) {
-    const HtmlEl = document.getElementsByTagName("html")[0],
-          themeClassPrefix = "theme-";
-
-    allThemes.forEach(themeItem => HtmlEl.classList.remove(`${themeClassPrefix}${themeItem}`));
-
-    HtmlEl.classList.add(`${themeClassPrefix}${theme}`);
   }
 
   function init() {
