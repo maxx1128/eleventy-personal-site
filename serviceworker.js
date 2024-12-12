912fff979232be87db9d9582ad9a1279397fccfb
maxx1128 <max.antonucci@inboxhealth.com>
2024-12-12 09:39:19 -0500

    Add CSS page transitions

--- serviceworker.js
+++ serviceworker.js
@@ -1,35 +1,35 @@
-const version = 'V3.01',
-  staticCacheName = `${version}staticfiles`
+const version = "V3.02",
+  staticCacheName = `${version}staticfiles`;
 
-addEventListener('install', (installEvent) => {
-  skipWaiting()
+addEventListener("install", (installEvent) => {
+  skipWaiting();
   installEvent.waitUntil(
     caches.open(staticCacheName).then((staticCache) => {
       // Nice to Cache
       staticCache.addAll([
-        '404.html',
-        'index.html',
-        'about/index.html',
-        'portfolio/index.html',
-        'blog/index.html',
-        'contact/index.html',
-        '/assets/images/global/favicon.ico',
-        '/assets/images/global/profile-2.jpg',
-        '/assets/images/icons/icon-192.png',
-        '/assets/images/icons/icon-512.png',
-      ])
+        "404.html",
+        "index.html",
+        "about/index.html",
+        "portfolio/index.html",
+        "blog/index.html",
+        "contact/index.html",
+        "/assets/images/global/favicon.ico",
+        "/assets/images/global/profile-2.jpg",
+        "/assets/images/icons/icon-192.png",
+        "/assets/images/icons/icon-512.png",
+      ]);
 
       // Need to Cache
       return staticCache.addAll([
-        '/assets/css/main.css',
-        '/assets/javascript/main.js',
-      ])
-    }),
-  )
-})
+        "/assets/css/main.css",
+        "/assets/javascript/main.js",
+      ]);
+    })
+  );
+});
 
 // For clearing out old caches
-addEventListener('activate', (activateEvent) => {
+addEventListener("activate", (activateEvent) => {
   activateEvent.waitUntil(
     // Clean the old caches up
     caches
@@ -39,20 +39,20 @@ addEventListener('activate', (activateEvent) => {
           cacheNames.map((cacheName) => {
             if (cacheName !== staticCacheName) {
               // Time to kill!
-              return caches.delete(cacheName)
+              return caches.delete(cacheName);
             }
-          }),
-        )
+          })
+        );
       })
-      .then(() => clients.claim()),
-  )
-})
+      .then(() => clients.claim())
+  );
+});
 
-addEventListener('fetch', (fetchEvent) => {
-  const request = fetchEvent.request
+addEventListener("fetch", (fetchEvent) => {
+  const request = fetchEvent.request;
   fetchEvent.respondWith(
     caches.match(request).then((responseFromCache) => {
-      return responseFromCache || fetch(request)
-    }),
-  )
-})
+      return responseFromCache || fetch(request);
+    })
+  );
+});
